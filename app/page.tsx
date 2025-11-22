cd2016684608477f7ff7b05d449c65a6df930c21
Hiroshi <hiroshi@hiroshi>
2025-11-22 18:28:39 -0500

    Fix: Add complete Phantom wallet type definition

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,9 @@ interface PhantomProvider {
   connect: () => Promise<{ publicKey: { toString: () => string } }>
   disconnect: () => Promise<void>
   publicKey?: { toString: () => string }
+  signTransaction?: (transaction: any) => Promise<any>
+  signAllTransactions?: (transactions: any[]) => Promise<any[]>
+  signMessage?: (message: Uint8Array) => Promise<{ signature: Uint8Array }>
 }
 
 declare global {
